905c16ae04835ea5fa9c2bbdb6410d4f1644aaf5
Emanuel Lapa <40744610+iLapaa@users.noreply.github.com>
2024-02-29 14:51:22 +0000

    Update button.script.ts URL

--- src/scripts/src/button.script.ts
+++ src/scripts/src/button.script.ts
@@ -5,7 +5,7 @@ window.onload = () => {
       const url: string[] = movie.getAttribute("href").split("/");
 
       const newButton =
-        "<a style='width: 100%' href='http://localhost:3000/watch/tv/" +
+        "<a style='width: 100%' href='http://easyplayapp.vercel.app/watch/tv/" +
         url[2] +
         "/1/1'><button style='border-radius: 10px; color: white; border: unset; background-color: #5447ff; padding: 10px; width: 100%'>easywatch</button></a>";
 
